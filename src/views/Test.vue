0bfa9ff29cb9318a335a2b193970714076a73b21
ZhangMin1998 <1464253423@qq.com>
2023-07-05 22:16:36 +0800

    获取路由

---
+++ src/views/Test.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="test">
+    <h1>This is an test page {{ name }}</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 't_est',
+  data () {
+    return {
+      name: 'zm'
+    }
+  }
+}
+</script>
+
+<!-- 获取路由
+<script>
+  import { getCurrentInstance } from 'vue'
+
+  export default {
+    setup () {
+      const { ctx } = getCurrentInstance()
+      console.log(ctx.$router.currentRoute.value)
+    }
+  }
+</script> -->
+
+<style lang="scss" scoped>
+.test{
+  color: pink;
+}
+</style>
