cb130225f35c3a5791c127dd1a2ca94274786b60
ZhangMin1998 <1464253423@qq.com>
2023-07-09 16:59:07 +0800

    组件库按需加载

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import datav from 'datav-screen-libs'
+import datavTestComponent from 'datav-screen-libs/src/components/Test/index'
+// import datavTestComponent2 from 'datav-screen-libs/src/components/Test2/index'
 
-createApp(App).use(store).use(router).use(datav).mount('#app')
+// app.js 19.61kib (3个组件 未按需加载)
+// app.js 19.53kib (1个组件 未按需加载)
+// app.js 17.49kib (1个组件 按需加载)
+createApp(App).use(store).use(router).use(datavTestComponent).mount('#app')
