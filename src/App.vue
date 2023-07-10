663e2a7f80866e49f4eb975b899905d30b385c76
ZhangMin1998 <1464253423@qq.com>
2023-07-11 00:49:25 +0800

    svg icon

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,13 @@
   <div class="icon-wrapper">
     <test-component></test-component>
     <!-- <svg width="30" height="30" viewBox="0 0 100 100" style="color: red">
-      <use href="#more"></use>
-    </svg> -->
-    <svg width="30" height="30" style="color: rgb(67, 201, 219)">
       <use href="#more2"></use>
-    </svg>
+    </svg> -->
+    <!-- <svg width="30" height="30" style="color: rgb(67, 201, 219)">
+      <use href="#more2"></use>
+    </svg> -->
+    <!-- <Icon name="more2" :style="{width:50, height: 50}"></Icon> -->
+    <Icon name="more2" class="common-icon"></Icon>
   </div>
   <!-- <test-component2></test-component2>
   <test-component3></test-component3> -->
@@ -41,5 +43,10 @@ nav {
 }
 .icon-wrapper{
   color: #42b983;
+}
+.common-icon{
+  width: 100px;
+  height: 100px;
+  color: pink;
 }
 </style>
