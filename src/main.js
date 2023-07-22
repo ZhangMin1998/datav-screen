96b3cdf223a492e87fc7f2c26dbfd9942a1c363f
ZhangMin1998 <1464253423@qq.com>
2023-07-23 01:59:41 +0800

    TopHeader开发

--- src/main.js
+++ src/main.js
@@ -2,12 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import dataView from '@jiaminghi/data-view'
 // 先不做按需加载
+// import datavComponent from 'imooc-datav-libs-dev'
 import datav from 'datav-screen-libs'
 // import datavTestComponent from 'datav-screen-libs/src/components/Test/index'
 // import datavTestComponent2 from 'datav-screen-libs/src/components/Test2/index'
+import Echarts from 'vue-echarts'
 
 // app.js 19.61kib (3个组件 未按需加载)
 // app.js 19.53kib (1个组件 未按需加载)
 // app.js 17.49kib (1个组件 按需加载)
-createApp(App).use(store).use(router).use(datav).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(dataView)
+  .use(datav)
+  .component('vue-echarts', Echarts)
+  .mount('#app')
