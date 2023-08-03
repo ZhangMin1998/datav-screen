b62b30510b251e8188f1373c0ca88c6d0447afe5
ZhangMin1998 <1464253423@qq.com>
2023-08-03 23:26:45 +0800

    行动态样式开发

--- src/views/table.vue
+++ src/views/table.vue
@@ -18,6 +18,13 @@ export default {
     const config = ref({})
     const headerData = ['姓名', '年龄', '城市']
     const headerStyle = [{ color: 'pink', width: '100px' }] // , width: '100px'
+    const rowStyle = [{ color: 'blue' }]
+    const rowBg = ['rgb(180,180,180)', 'rgb(155,155,155)']
+    const aligns = ['center', 'center', 'center']
+    const headerFontSize = 24
+    const rowFontSize = 20
+    const headerColor = 'pink'
+    const rowColor = '#fff'
     const data = [
       ['小明', 28, '深圳'],
       ['小白', 38, '广州'],
@@ -35,11 +42,18 @@ export default {
     config.value = {
       headerData,
       headerStyle,
+      rowStyle,
       headerBg: '#999',
       headerHeight: 40,
       headerIndex: true,
       data,
-      rowNum: 10
+      rowNum: 10,
+      rowBg,
+      aligns,
+      headerFontSize,
+      rowFontSize,
+      headerColor,
+      rowColor
     }
     return {
       config
